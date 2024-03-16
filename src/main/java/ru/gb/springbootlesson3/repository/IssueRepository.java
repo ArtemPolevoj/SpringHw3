a3009c8c4ae85aa3d803d605cc1f35aa20e0a577
artem <artempolevoj@yandex.ru>
2024-03-16 19:48:41 +0300

    update IssueRepository

--- src/main/java/ru/gb/springbootlesson3/repository/IssueRepository.java
+++ src/main/java/ru/gb/springbootlesson3/repository/IssueRepository.java
@@ -3,20 +3,48 @@ package ru.gb.springbootlesson3.repository;
 import org.springframework.stereotype.Repository;
 import ru.gb.springbootlesson3.entity.Issue;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class IssueRepository {
-    private List<Issue> list = new ArrayList<>();
+    private final List<Issue> list = new ArrayList<>();
 
-    public void createIssue(Issue issue){
+    public void createIssue(Issue issue) {
         list.add(issue);
     }
 
-    public Issue findById(long id){
+    public Issue findById(long id) {
         return list.stream().filter(e -> e.getId() == id)
                 .findFirst()
                 .orElse(null);
+    }
+
+    public int getCountBookReader(long readerId) {
+        int count = 0;
+        for (Issue issue:list){
+            if (issue.getIdReader() == readerId && issue.getReturnedAt() == null){
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public List<Issue> findIssueByReaderId(long readerId){
+        List<Issue> temp = new ArrayList<>();
+        for (Issue issue:list){
+            if (issue.getIdReader() == readerId){
+                temp.add(issue);
+            }
+        }
+        return temp;
+    }
+    public void setReturnedAt(long id){
+        for (Issue issue:list){
+            if (issue.getId() == id && issue.getReturnedAt() == null){
+                issue.setReturnedAt(LocalDateTime.now());
+            }
+        }
     }
 }
